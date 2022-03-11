5543fd2347cd8e1e6983684a3135b03f96359b3c
GDN\syntia <syntia@gdn-commerce.com>
2022-03-12 00:15:14 +0700

    Add data

--- app/src/main/java/com/refillution/core/data/local/ProductData.kt
+++ app/src/main/java/com/refillution/core/data/local/ProductData.kt
@@ -37,6 +37,37 @@ object ProductData {
                 500 to 20000.0,
                 1000 to 40000.0
             )
+        ),
+        ProductDetail(
+            id = "dfdd27e1",
+            name = "Daia Violet Deterjen",
+            price = 20000.0,
+            image = "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//98/MTA-3790429/daia_detergent-daia-1-8kg-bks-purple_full02.jpg",
+            brand = "Daia",
+            category = "Kebutuhan rumah tangga",
+            unitType = "Berat",
+            unit = "gram",
+            maxUnit = "kg",
+            measurements = mapOf(
+                500 to 20000.0,
+                1000 to 34000.0
+            )
+        ),
+        ProductDetail(
+            id = "204de14a",
+            name = "Head & Shoulders Shampoo Lemon Fresh",
+            price = 20000.0,
+            image = "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//95/MTA-26084281/head_-_shoulders_head_-_shoulders_shampoo_anti_ketombe_lemon_fresh_850ml_full01_gd633cjg.jpg",
+            brand = "Head & Shoulders",
+            category = "Kebutuhan rumah tangga",
+            unitType = "Volume",
+            unit = "ml",
+            maxUnit = "liter",
+            measurements = mapOf(
+                250 to 20000.0,
+                500 to 35000.0,
+                1000 to 68000.0
+            )
         )
     )
   }
